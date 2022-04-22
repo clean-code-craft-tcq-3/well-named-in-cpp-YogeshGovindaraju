52f7a6187d983255c5d7827f13e4c48181e30796
YogeshGovindaraju <99166617+YogeshGovindaraju@users.noreply.github.com>
2022-04-22 13:30:05 +0530

    Add files via upload

---
+++ ColorCodeReferenceManual.h
@@ -0,0 +1,11 @@
+#ifndef COLORCODEREFERENCEMANUAL
+#define COLORCODEREFERENCEMANUAL
+
+#include "ColorCode.h"
+
+namespace TelecommunicationColorCoder
+{
+    void printReferenceManual();
+}
+
+#endif
